535cd7db4b8c3710d5004f86022b84488fbf43e1
nullbox <nullbox@local>
2019-09-13 22:20:54 +0300

    cleanup

--- app/code/local/Aristos/Test/controllers/IndexController.php
+++ app/code/local/Aristos/Test/controllers/IndexController.php
@@ -2,21 +2,8 @@
 
 class Aristos_Test_IndexController extends Mage_Core_Controller_Front_Action {
 	public function indexAction() {
-		//remove our previous echo
-//		echo 'Hello Index!';
+
 		$this->loadLayout();
 		$this->renderLayout();
-	}
-	public function goodbyeAction() {
-		$this->loadLayout();
-		$this->renderLayout();
-	}
-	public function paramsAction() {
-		echo '<dl>';
-		foreach($this->getRequest()->getParams() as $key=>$value) {
-			echo '<dt><strong>Param:</strong>'.$key.'</dt>';
-			echo '<dt><strong>Value: </strong>'.$value.'</dt>';
-		}
-		echo '</dl>';
 	}
 }
--- app/code/local/Aristos/Test/controllers/TestController.php
+++ app/code/local/Aristos/Test/controllers/TestController.php
@@ -1,16 +1,7 @@
 <?php
 
 class Aristos_Test_TestController extends Mage_Core_Controller_Front_Action {
-	public function goodbyeAction() {
-		echo 'Aristos TEST Goodbye';
-	}
-	
 	public function indexAction() {
-		$this->loadLayout();
-		$this->renderLayout();
-	}
-	
-	public function testAction() {
 		$this->loadLayout();
 		$this->renderLayout();
 	}
